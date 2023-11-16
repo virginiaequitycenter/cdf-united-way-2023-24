ce6d26387bfc9aae5abc5d158aab618cb3456c41
eam5 <eam5hc@virginia.edu>
2023-11-16 16:23:15 -0500

    update r script

--- united-way/tidycensus-acs-example.R
+++ united-way/tidycensus-acs-example.R
@@ -101,28 +101,12 @@ region <- ccode$code # list of desired counties
 # variables: define varlist
 varlist_s = c("S1701_C03_001", # povrate
               "S1701_C03_002",   # cpovrate
-              "S1901_C01_012",   # hhinc
-              "S1501_C02_014",   # hsmore
-              "S1501_C02_015",   # bamore
-              "S1501_C02_013",   # gradmore
-              "S2301_C04_001",   # unemp
-              "S2701_C03_001",   # hlthins
-              "S2704_C03_001",   # pubins
-              "S2001_C01_002")   # earn
+              "S1901_C01_012")   # hhinc
+              
 
 varlist_b = c("B01003_001", # totalpop
-              "B19083_001",  # gini
-              "B25070_007",  # 30-35 rent burdened
-              "B25070_008",  # 35-40 rent burdened
-              "B25070_009",  # 40-50 rent burdened
-              "B25070_010",  # 50+ rent burdened
-              "B25070_001",  # all renters
-              "B25003_002",  # owner-occupied housing units
-              "B25003_001",  # occupied housing units
-              "B25002_003",  # vacant housing units
               "B25002_001",  # housing units
-              "B19058_002",  # SNAP
-              "B05002_013")  # Foreign-born
+              "B19058_002")  # SNAP
 
 
 # Pull variables
@@ -146,61 +130,15 @@ county_data_b <- get_acs(geography = "county",
 names(county_data_s) = c("GEOID", "NAME",
                          "povrateE", "povrateM",
                          "cpovrateE", "cpovrateM",
-                         "hhincE", "hhincM",
-                         "hsmoreE", "hsmoreM",
-                         "bamoreE", "bamoreM",
-                         "gradmoreE", "gradmoreM",
-                         "unempE", "unempM",
-                         "hlthinsE", "hlthinsM",
-                         "pubinsE", "pubinsM",
-                         "earnE", "earnM")
+                         "hhincE", "hhincM")
 
 names(county_data_b) = c("GEOID", "NAME",  
                          "totalpopE", "totalpopM",
-                         "giniE", "giniM",
-                         "rent30E", "rent30M",
-                         "rent35E", "rent35M",
-                         "rent40E", "rent40M",
-                         "rent50E", "rent50M",
-                         "rentallE", "rentallM",
-                         "ownoccE", "ownoccM",
-                         "occhseE", "occhseM",
-                         "vachseE", "vachseM",
                          "allhseE", "allhseM",
-                         "snapE", "snapM",
-                         "foreignbE", "foreignbM")
-
-# Derive some variables
-county_data_b <- county_data_b %>% 
-  mutate(rentersumE = (rent30E+rent35E+rent40E+rent50E),
-         rentersumM = (rent30M/1.645)^2 + (rent35M/1.645)^2 + (rent40M/1.645)^2 + (rent50M/1.645)^2,
-         rentersumM = sqrt(rentersumM)*1.645,
-         renter30E = round((rentersumE/rentallE)*100,1),
-         renter30M = moe_prop(rentersumE, rentallE, rentersumM, rentallM),
-         renter30M = round(renter30M*100,1)) %>% 
-  mutate(homeownE = round((ownoccE/occhseE)*100,1),
-         homeownM = moe_prop(ownoccE, occhseE, ownoccM, occhseM),
-         homeownM = round(homeownM*100, 1)) %>% 
-  mutate(vacrateE = round((vachseE/allhseE)*100,1),
-         vacrateM = moe_prop(vachseE, allhseE, vachseM, allhseM),
-         vacrateM = round(vacrateM*100, 1)) %>% 
-  select(-c(rentersumE, rentersumM,rent30E:occhseM))
-
-# Derive snap variables
-county_data_b <- county_data_b %>% 
-  mutate(perc_snaphseE = round((snapE / allhseE)*100,1),
-         perc_snaphseM = round(moe_prop(snapE, allhseE, snapM, allhseM), 2),
-         .keep = "all")
-
-# Derive foreign born variables
-county_data_b <- county_data_b %>% 
-  mutate(perc_forbE = round((foreignbE / totalpopE)*100,1),
-         perc_forbM = round(moe_prop(foreignbE, totalpopE, foreignbM, totalpopM), 2),
-         .keep = "all")
-
+                         "snapE", "snapM")
 
 # Get Data
-# pull tables (easier to just pull tables separately)
+# pull tables (easier to just pull these tables separately)
 county_race <- get_acs(geography = "county", 
                        table = "DP05", 
                        state = "VA", 
@@ -208,30 +146,8 @@ county_race <- get_acs(geography = "county",
                        survey = "acs5",
                        year = 2021)
 
-county_age <- get_acs(geography = "county", 
-                      table = "S0101", 
-                      state = "VA", 
-                      county = region, 
-                      survey = "acs5",
-                      year = 2021)
-
-county_enroll <- get_acs(geography = "county", 
-                         table = "S1401", 
-                         state = "VA", 
-                         county = region, 
-                         survey = "acs5", 
-                         year = 2021)
-
-county_disability <- get_acs(geography = "county", 
-                             table = "C18130", 
-                             state = "VA", 
-                             county = region, 
-                             survey = "acs5",
-                             year = 2021) 
-
-
 # ....................................................
-# 3. Metrics specific to locality level  ----
+# 3. Metrics specific to locality level - Household Income by race  ----
 varlist_race <- c("B19013B_001",  # hhinc_black
                   "B19013D_001",  # hhinc_asion
                   "B19013G_001",  # hhinc_multi
@@ -256,35 +172,6 @@ names(county_hhinc_race) = c("GEOID", "NAME",
 
 # ....................................................
 # 4. Reduce and Combine data ----
-
-# Reduce tables: tract_age, tract_race, tract_enroll
-# tract_age: three age groups present as rows in the table,
-#             one group must be summed
-county_age17 <- county_age %>% 
-  filter(variable == "S0101_C02_022") %>% 
-  rename(age17E = estimate,
-         age17M = moe) %>% 
-  select(-variable)
-
-county_age24 <- county_age %>% 
-  filter(variable == "S0101_C02_023") %>% 
-  rename(age24E = estimate,
-         age24M = moe) %>% 
-  select(-variable)
-
-county_age64 <- county_age %>% 
-  filter(variable %in% c("S0101_C02_007", "S0101_C02_008", "S0101_C02_009",
-                         "S0101_C02_010", "S0101_C02_011", "S0101_C02_012",
-                         "S0101_C02_013", "S0101_C02_014")) %>% 
-  group_by(GEOID, NAME) %>% 
-  summarize(age64E = sum(estimate),
-            age64M = moe_sum(moe = moe, estimate = estimate))
-
-county_age65 <- county_age %>% 
-  filter(variable == "S0101_C02_030") %>% 
-  rename(age65E = estimate,
-         age65M = moe) %>% 
-  select(-variable)
 
 # tract_race: all groups present as rows in the table
 #             some other race and native hawaiian/pacific islander combined
@@ -331,59 +218,26 @@ county_ltnx <- county_race %>%
          ltnxM = moe) %>% 
   select(-variable)
 
-# tract_schl: 6 groups (3-4, 5-9, 10-14, 15-17, 18-19, 20-24) must be summed
-#             population and enrolled, and divided
-county_schl_num <- county_enroll %>% 
-  filter(variable %in% c("S1401_C01_014", "S1401_C01_016", "S1401_C01_018", "S1401_C01_020", "S1401_C01_022", "S1401_C01_024")) %>% 
-  group_by(GEOID, NAME) %>% 
-  summarize(schl_num = sum(estimate), 
-            schl_numM = moe_sum(moe = moe, estimate = estimate))
-
-county_schl_den <- county_enroll %>% 
-  filter(variable %in% c("S1401_C01_013", "S1401_C01_015", "S1401_C01_017", "S1401_C01_019", "S1401_C01_021", "S1401_C01_023")) %>% 
-  group_by(GEOID, NAME) %>% 
-  summarize(schl_den = sum(estimate), 
-            schl_denM = moe_sum(moe = moe, estimate = estimate))
-
-county_schl_ratio <- left_join(county_schl_num, county_schl_den)
-
-county_schl <- county_schl_ratio %>% 
-  summarize(schlE = round((schl_num/schl_den)*100, 1),
-            schlM = moe_prop(schl_num, schl_den, schl_numM, schl_denM),
-            schlM = round(schlM*100,1))
-
-county_dis <- county_disability %>%
-  filter(variable == "C18130_003" | variable == "C18130_010" | variable == "C18130_017") %>%
-  group_by(GEOID, NAME) %>% 
-  summarize(disability_numE = sum(estimate), 
-            disability_numM = moe_sum(moe = moe, estimate = estimate))
-
 
 # Combine indicators
 # joining columns
 county_data <- county_data_s %>% 
-  left_join(county_data_b) %>% 
-  left_join(county_schl) %>% 
+  left_join(county_data_b) %>%
   left_join(county_white) %>% 
   left_join(county_black) %>% 
   left_join(county_indig) %>% 
   left_join(county_asian) %>% 
   left_join(county_othrace) %>% 
   left_join(county_multi) %>% 
-  left_join(county_ltnx) %>% 
-  left_join(county_age17) %>% 
-  left_join(county_age24) %>% 
-  left_join(county_age64) %>% 
-  left_join(county_age65) %>% 
-  left_join(county_hhinc_race) %>%
-  left_join(county_dis)
+  left_join(county_ltnx) %>%
+  left_join(county_hhinc_race)
 
 county_data <- county_data %>% 
   mutate(geoid = GEOID,
          year = "2021") %>% 
   separate(geoid, into = c("state", "locality"), 
            sep = c(2)) %>% 
-  select(GEOID, NAME, year, totalpopE, totalpopM, whiteE, whiteM, blackE, blackM, asianE, asianM, indigE, indigM, othraceE, othraceM, multiE, multiM, ltnxE, ltnxM, everything())
+  select(GEOID, state, locality, NAME, year, totalpopE, totalpopM, whiteE, whiteM, blackE, blackM, asianE, asianM, indigE, indigM, othraceE, othraceM, multiE, multiM, ltnxE, ltnxM, everything())
 
 
 # ....................................................
@@ -394,11 +248,9 @@ county_data %>% select_at(vars(ends_with("E"))) %>% summary()
 # ....................................................
 # 6. Save ----
 # save to RDS
-saveRDS(county_data, file = "data/county_data.RDS") 
+saveRDS(county_data, file = "data/county_data_example.RDS") 
 
 # or save to csv
-# write.csv(county_data, "county_data.csv", row.names=FALSE)
-
-
+write.csv(county_data, "data/county_data_example.csv", row.names=FALSE)
 
 
